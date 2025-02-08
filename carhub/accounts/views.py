7a1c511cae6302652623e47fed3209f60a622b9a
maxybyte <tolesy2003@outlook.com>
2025-02-08 11:56:59 +0200

    add html files for login and registration and register templates in project settings

--- carhub/accounts/views.py
+++ carhub/accounts/views.py
@@ -12,46 +12,44 @@ def register(request):
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
 
-        # Checking if username already exists
-        if User.objects.filter(username=user_name).exists():
-            messages.error(request, 'Username already exists!')
+        if password == confirm_password:
+            if User.objects.filter(username=user_name).exists():
+                messages.error(request, 'Username already exists!')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already in use!')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=user_name,  password=password)
+                    auth.login(request, user)
+                    messages.success(request, 'You are now logged in')
+                    return redirect('dashboard')
+                    user.save()
+                    messages.success(request, 'You have successfully registered')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
             return redirect('register')
+    return render(request, 'accounts/register.html')
 
-        # Checking if email already exists
-        if User.objects.filter(email=email).exists():
-            messages.error(request, 'Email already in use!')
-            return redirect('register')
 
-        # Checking if passwords match
-        if password != confirm_password:
-            messages.error(request, 'Passwords do not match!')
-            return redirect('register')
-
-        # Creating the user
-        user = User.objects.create_user(username=user_name, email=email, password=password)
-        user.first_name = first_name
-        user.last_name = last_name
-        user.save()
-
-        messages.success(request, 'Registration successful! You can now log in.')
-        return redirect('login')
-
-    return render(request, 'register.html')
 
 def login(request):
     if request.method == 'POST':
-        user_name = request.POST['username']
+        username = request.POST['username']
         password = request.POST['password']
 
-        user = auth.authenticate(username=user_name, password=password)
+        user = auth.authenticate(username=username, password=password)
 
         if user is not None:
-            login(request, user)
+            auth.login(request, user)
             messages.success(request, 'You are now logged in!')
-            return redirect('home')
+            return redirect('dashboard')
         else:
             messages.error(request, 'Invalid username or password!')
             return redirect('login')
+    return render(request, 'accounts/login.html')
 
 
 
