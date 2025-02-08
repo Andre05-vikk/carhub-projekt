baf9cbf5c50a0daf725b5abb0619b83ae720aae3
maxybyte <tolesy2003@outlook.com>
2025-02-08 12:22:57 +0200

    add migration and fix the registration logic

--- carhub/accounts/views.py
+++ carhub/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib import messages
+from django.contrib import messages, auth
 
 # Register function
 def register(request):
