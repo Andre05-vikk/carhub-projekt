e0a09af415e5c6d8566606a64d4e2b5705a6d4a7
AimarToomela <toomelaaimar@gmail.com>
2025-02-08 14:37:12 +0200

    add contact model, update views, urls; add contact.html

--- carhub/pages/views.py
+++ carhub/pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Team
+from .models import Team, Contact
 
 
 # Create your views here.
@@ -12,6 +12,13 @@ def home(request):
 
     return render(request, 'pages/home.html', data)
 
+
+def contact(request):
+    contacts = Contact.objects.all()
+    data = {
+        'contacts': contacts,
+    }
+    return render(request, 'pages/contact.html', data)
 
 # contact - Aimar
 # services -  Sander
