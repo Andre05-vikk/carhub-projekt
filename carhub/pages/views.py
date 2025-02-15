ac84f1c85768ec76dacc1391f292de3d670b97d5
maxybyte <tolesy2003@outlook.com>
2025-02-15 11:49:12 +0200

    progressing

--- carhub/pages/views.py
+++ carhub/pages/views.py
@@ -3,14 +3,29 @@ from .models import Team
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
 from django.contrib import messages
+from cars.models import Car
 
 
 # Create your views here.
 def home(request):
     teams = Team.objects.all()
+    featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
+    all_cars = Car.objects.order_by('-created_date')
+    car_title_search = Car.objects.values_list('car_title', flat=True).distinct()
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    state_search = Car.objects.values_list('state', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
 
     data = {
         'teams': teams,
+        'featured_cars': featured_cars,
+        'all_cars': all_cars,
+        'car_title_search': car_title_search,
+        'model_search': model_search,
+        'state_search': state_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
     }
 
     return render(request, 'pages/home.html', data)
